0c8a7c141830f19f1fe35b0d58d189f8052f17f9
Tran Anh Tuan <tk1cntt@gmail.com>
2018-03-29 19:26:27 +0700

    Add tab navigation

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,29 +1,48 @@
 import React, { Component } from 'react'
 import { ScrollView, Text, Image, View } from 'react-native'
 import { Images } from '../Themes'
+import BottomNavigation, { Tab } from 'react-native-material-bottom-navigation'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 // Styles
 import styles from './Styles/LaunchScreenStyles'
 
 export default class LaunchScreen extends Component {
-  render () {
+  render() {
     return (
-      <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
-        <ScrollView style={styles.container}>
-          <View style={styles.centered}>
-            <Image source={Images.launch} style={styles.logo} />
-          </View>
-
-          <View style={styles.section} >
-            <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
-            </Text>
-          </View>
-
-        </ScrollView>
-      </View>
+      <BottomNavigation
+        labelColor="white"
+        rippleColor="white"
+        style={{
+          height: 56,
+          elevation: 8,
+          position: 'absolute',
+          left: 0,
+          bottom: 0,
+          right: 0
+        }}
+      >
+        <Tab
+          barBackgroundColor="#37474F"
+          label="Home"
+          icon={<Icon size={24} color="white" name="home" />}
+        />
+        <Tab
+          barBackgroundColor="#00796B"
+          label="Scorecards"
+          icon={<Icon size={24} color="white" name="speaker-notes" />}
+        />
+        <Tab
+          barBackgroundColor="#5D4037"
+          label="History"
+          icon={<Icon size={24} color="white" name="insert-invitation" />}
+        />
+        <Tab
+          barBackgroundColor="#3E2723"
+          label="Menu"
+          icon={<Icon size={24} color="white" name="reorder" />}
+        />
+      </BottomNavigation>
     )
   }
 }
