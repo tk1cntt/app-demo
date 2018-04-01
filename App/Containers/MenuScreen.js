83d355ceacc2dee27a2536f16175b389af7571d2
Tran Anh Tuan <tk1cntt@gmail.com>
2018-04-01 18:46:45 +0700

    Add menu icon

--- App/Containers/MenuScreen.js
+++ App/Containers/MenuScreen.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import { View, Text, FlatList } from 'react-native'
 import { connect } from 'react-redux'
-import Icon from 'react-native-vector-icons/MaterialIcons'
+import { Container, Header, Content, List, ListItem, Text, Icon, Left, Body, Right, Separator } from 'native-base';
 
 // More info here: https://facebook.github.io/react-native/docs/flatlist.html
 import MenuItem from '../Components/MenuItem'
@@ -13,97 +12,116 @@ class MenuScreen extends Component {
 
   static navigationOptions = {
     tabBarLabel: 'Menu',
-    tabBarIcon: () => <Icon size={24} color="white" name="reorder" />
+    tabBarIcon: () => <Icon ios='ios-menu' android="md-menu" style={{fontSize: 24, color: 'white'}}/>
   }
-  /* ***********************************************************
-  * STEP 1
-  * This is an array of objects with the properties you desire
-  * Usually this should come from Redux mapStateToProps
-  *************************************************************/
-  state = {
-    dataObjects: [
-      {title: 'Home', icon: 'home'},
+
+/*
+  {title: 'Home', icon: 'home'},
       {title: 'User profile', icon: 'home'},
       {title: 'History', icon: 'insert-invitation'},
       {title: 'Account', icon: 'insert-invitation'},
       {title: 'ScoreCards', icon: 'speaker-notes'},
       {title: 'Friends', icon: 'insert-invitation'},
       {title: 'Member', icon: 'insert-invitation'}
-    ]
-  }
-
-  /* ***********************************************************
-  * STEP 2
-  * `renderRow` function. How each cell/row should be rendered
-  * It's our best practice to place a single component here:
-  *
-  * e.g.
-    return <MyCustomCell title={item.title} description={item.description} />
-  *************************************************************/
-  renderRow ({item}) {
+*/
+  render() {
     return (
-      <MenuItem item={item} />
-    )
-  }
-
-  /* ***********************************************************
-  * STEP 3
-  * Consider the configurations we've set below.  Customize them
-  * to your liking!  Each with some friendly advice.
-  *************************************************************/
-  // Render a header?
-  renderHeader = () =>
-    <Text style={[styles.label, styles.sectionHeader]}> - MENU - </Text>
-
-  // Render a footer?
-  renderFooter = () =>
-    <Text style={[styles.label, styles.sectionHeader]}> - Footer - </Text>
-
-  // Show this when data is empty
-  renderEmpty = () =>
-    <Text style={styles.label}> - Nothing to See Here - </Text>
-
-  renderSeparator = () =>
-    <Text style={styles.label}> - ~~~~~ - </Text>
-
-  // The default function if no Key is provided is index
-  // an identifiable key is important if you plan on
-  // item reordering.  Otherwise index is fine
-  keyExtractor = (item, index) => index
-
-  // How many items should be kept im memory as we scroll?
-  oneScreensWorth = 20
-
-  // extraData is for anything that is not indicated in data
-  // for instance, if you kept "favorites" in `this.state.favs`
-  // pass that in, so changes in favorites will cause a re-render
-  // and your renderItem will have access to change depending on state
-  // e.g. `extraData`={this.state.favs}
-
-  // Optimize your list if the height of each item can be calculated
-  // by supplying a constant height, there is no need to measure each
-  // item after it renders.  This can save significant time for lists
-  // of a size 100+
-  // e.g. itemLayout={(data, index) => (
-  //   {length: ITEM_HEIGHT, offset: ITEM_HEIGHT * index, index}
-  // )}
-
-  render () {
-    return (
-      <View style={styles.container}>
-        <FlatList
-          contentContainerStyle={styles.listContent}
-          data={this.state.dataObjects}
-          renderItem={this.renderRow}
-          keyExtractor={this.keyExtractor}
-          initialNumToRender={this.oneScreensWorth}
-          ListHeaderComponent={this.renderHeader}
-          ListFooterComponent={this.renderFooter}
-          ListEmptyComponent={this.renderEmpty}
-          ItemSeparatorComponent={this.renderSeparator}
-        />
-      </View>
-    )
+      <Container>
+        <Header />
+        <Content>
+          <List>
+            <ListItem itemDivider>
+              <Text>Menu</Text>
+            </ListItem>
+            <ListItem icon>
+              <Left>
+                <Icon name="home" />
+              </Left>
+              <Body>
+                <Text>Home</Text>
+              </Body>
+            </ListItem>
+            <ListItem icon>
+              <Left>
+                <Icon name="person" />
+              </Left>
+              <Body>
+                <Text>User profile</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </ListItem>
+            <ListItem icon>
+              <Left>
+                <Icon name="calendar" />
+              </Left>
+              <Body>
+                <Text>History</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </ListItem>
+            <ListItem icon>
+              <Left>
+                <Icon name="card" />
+              </Left>
+              <Body>
+                <Text>Account</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </ListItem>
+            <ListItem icon>
+              <Left>
+                <Icon name="list-box" />
+              </Left>
+              <Body>
+                <Text>Scorecards</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </ListItem>
+            <ListItem icon>
+              <Left>
+                <Icon name="contacts" />
+              </Left>
+              <Body>
+                <Text>Friends</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </ListItem>
+            <ListItem icon>
+              <Left>
+                <Icon name="ribbon" />
+              </Left>
+              <Body>
+                <Text>Members</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </ListItem>
+            <ListItem itemDivider>
+              <Text>Terms of service</Text>
+            </ListItem>
+            <ListItem icon>
+              <Body>
+                <Text>Terms and Policies</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+    );
   }
 }
 
